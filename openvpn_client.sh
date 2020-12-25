53fc0de1f52bdf3d322bb9e33d78d207b7d8b2d1
Jakub Siska <46302531+siskajakub@users.noreply.github.com>
2020-12-26 00:23:00 +0100

    Improved logging

--- openvpn_client.sh
+++ openvpn_client.sh
@@ -11,55 +11,59 @@
 # variables
 TUN_DEVICE="/dev/net/tun" # tun device
 PID_FILE="/etc/openvpn/client/openvpn_client.pid" # openvpn pid file
+LOG_FILE="/etc/openvpn/client/openvpn_client.log" # openvpn log file
 OPENVPN_CONFIG="/etc/openvpn/client/config/" # folder with openvpn config files
 OPENVPN_AUTH="/etc/openvpn/client/auth.txt" # openvpn authentication file
 
+# reset log file
+echo "INFO: log has been initiated." |& tee $LOG_FILE
 
 # check for tunnel device being present
 if [[ -c $TUN_DEVICE ]]
 then
-	echo "tun device exists."
+	echo "INFO: tun device exists." |& tee -a $LOG_FILE
 else
 	# create tun device
-	echo "tun device does not exist, creating..."
+	echo "INFO: tun device does not exist, creating..." |& tee -a $LOG_FILE
 	mkdir /dev/net
 	mknod /dev/net/tun c 10 200
 	chmod 666 /dev/net/tun
-	echo "tun device has been created."
+	echo "INFO: tun device has been created." |& tee -a $LOG_FILE
 fi
 
 
 # stop dante service (socks server)
-echo "stopping dante service..."
-systemctl stop danted
-echo "dante service has been stopped."
+echo "INFO: stopping dante service..." |& tee -a $LOG_FILE
+systemctl kill danted
+echo "INFO: dante service has been stopped." |& tee -a $LOG_FILE
+systemctl status danted |& tee -a $LOG_FILE
 
 
 # check for openvpn process from PID file, if exists, kill
 if [[ -f $PID_FILE ]]
 then
 	PID=$(< $PID_FILE)
-	echo "openvpn pid is $PID, now we are going to kill it."
+	echo "INFO: openvpn pid is $PID, now we are going to kill it." |& tee -a $LOG_FILE
 	kill $PID
 	while [[ $(kill -0 $PID) ]]
 	do
 		sleep .1s
 	done
 	rm $PID_FILE
-	echo "openvpn pid $PID has been terminated."
+	echo "INFO: openvpn pid $PID has been terminated." |& tee -a $LOG_FILE
 else
-	echo "no openvpn pid."
+	echo "INFO: no openvpn pid file." |& tee -a $LOG_FILE
 fi
 
 
 # select random openvpn config file
 OPENVPN_CONFIG_FILE=$(ls $OPENVPN_CONFIG -A1 | grep \.ovpn$ | sort -R | head -1)
-echo "openvpn configuration $OPENVPN_CONFIG_FILE has been selected."
+echo "INFO: openvpn configuration $OPENVPN_CONFIG_FILE has been selected." |& tee -a $LOG_FILE
 
 
 # start new openvpn connection
-echo "starting new openvpn session..."
-/usr/sbin/openvpn --config $OPENVPN_CONFIG$OPENVPN_CONFIG_FILE --auth-user-pass $OPENVPN_AUTH & echo $! > $PID_FILE
+echo "INFO: starting new openvpn session..." |& tee -a $LOG_FILE
+/usr/sbin/openvpn --config $OPENVPN_CONFIG$OPENVPN_CONFIG_FILE --auth-user-pass $OPENVPN_AUTH --writepid $PID_FILE --log-append $LOG_FILE &
 
 
 # wait until openvpn connects and then restart dante (socks server)
@@ -72,6 +76,7 @@ do
 	sleep .1s
 	((I--))
 done
-echo "restarting dante service..."
+echo "INFO: restarting dante service..." |& tee -a $LOG_FILE
 systemctl restart danted
-echo "dante service has been restarted."
+echo "INFO: dante service has been restarted." |& tee -a $LOG_FILE
+systemctl status danted |& tee -a $LOG_FILE
